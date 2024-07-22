a4bd4e126ef5fa2114855d263afeb6a5c9da8252
Deepak2241993 <deepakprasad224@gmail.com>
2024-07-22 13:18:59 +0530

    details page update

--- resources/views/product/checkout.blade.php
+++ resources/views/product/checkout.blade.php
@@ -40,21 +40,7 @@ $amount=0;
                    <div class="checkbox-form">
                       <h3 class="mb-15">Billing Details</h3>
                       <div class="row g-5">
-                         <div class="col-md-12">
-                            <div class="country-select">
-                               <label>Country <span class="required">*</span></label>
-                               <select>
-                                  <option value="volvo">United States</option>
-                                  <option value="saab">Algeria</option>
-                                  <option value="mercedes">Afghanistan</option>
-                                  <option value="audi">Ghana</option>
-                                  <option value="audi2">Albania</option>
-                                  <option value="audi3">Bahrain</option>
-                                  <option value="audi4">Colombia</option>
-                                  <option value="audi5">Dominican Republic</option>
-                               </select>
-                            </div>
-                         </div>
+           
                          <div class="col-md-6">
                             <div class="checkout-form-list">
                                <label>First Name <span class="required">*</span></label>
@@ -67,12 +53,7 @@ $amount=0;
                                <input type="text" placeholder="">
                             </div>
                          </div>
-                         <div class="col-md-12">
-                            <div class="checkout-form-list">
-                               <label>Company Name</label>
-                               <input type="text" placeholder="">
-                            </div>
-                         </div>
+    
                          <div class="col-md-12">
                             <div class="checkout-form-list">
                                <label>Address <span class="required">*</span></label>
@@ -114,111 +95,9 @@ $amount=0;
                                <input type="text" placeholder="Postcode / Zip">
                             </div>
                          </div>
-                         <div class="col-md-12">
-                            <div class="checkout-form-list create-acc d-flex align-content-center">
-                               <input class="e-check-input" id="xbox" type="checkbox">
-                               <label class="mb-0">Create an account?</label>
-                            </div>
-                            <div id="cbox_info" class="checkout-form-list create-account">
-                               <p>Create an account by entering the information below. If you are a
-                                  returning
-                                  customer please login at the top of the page.</p>
-                               <label>Account password <span class="required">*</span></label>
-                               <input type="password" placeholder="password">
-                            </div>
-                         </div>
+           
                       </div>
-                      <div class="different-address">
-                         <div class="ship-different-title">
-                            <label>Ship to a different address?</label>
-                            <input class="e-check-input" id="ship-box" type="checkbox">
-                         </div>
-                         <div id="ship-box-info">
-                            <div class="row">
-                               <div class="col-md-12">
-                                  <div class="country-select">
-                                     <label>Country <span class="required">*</span></label>
-                                     <select>
-                                        <option value="volvo">Bangladesh</option>
-                                        <option value="saab">Algeria</option>
-                                        <option value="mercedes">Afghanistan</option>
-                                        <option value="audi">Ghana</option>
-                                        <option value="audi2">Albania</option>
-                                        <option value="audi3">Bahrain</option>
-                                        <option value="audi4">Colombia</option>
-                                        <option value="audi5">Dominican Republic</option>
-                                     </select>
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>First Name <span class="required">*</span></label>
-                                     <input type="text" placeholder="">
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>Last Name <span class="required">*</span></label>
-                                     <input type="text" placeholder="">
-                                  </div>
-                               </div>
-                               <div class="col-md-12">
-                                  <div class="checkout-form-list">
-                                     <label>Company Name</label>
-                                     <input type="text" placeholder="">
-                                  </div>
-                               </div>
-                               <div class="col-md-12">
-                                  <div class="checkout-form-list">
-                                     <label>Address <span class="required">*</span></label>
-                                     <input type="text" placeholder="Street address">
-                                  </div>
-                               </div>
-                               <div class="col-md-12">
-                                  <div class="checkout-form-list">
-                                     <input type="text" placeholder="Apartment, suite, unit etc. (optional)">
-                                  </div>
-                               </div>
-                               <div class="col-md-12">
-                                  <div class="checkout-form-list">
-                                     <label>Town / City <span class="required">*</span></label>
-                                     <input type="text" placeholder="Town / City">
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>State / County <span class="required">*</span></label>
-                                     <input type="text" placeholder="">
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>Postcode / Zip <span class="required">*</span></label>
-                                     <input type="text" placeholder="Postcode / Zip">
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>Email Address <span class="required">*</span></label>
-                                     <input type="email" placeholder="">
-                                  </div>
-                               </div>
-                               <div class="col-md-6">
-                                  <div class="checkout-form-list">
-                                     <label>Phone <span class="required">*</span></label>
-                                     <input type="text" placeholder="Postcode / Zip">
-                                  </div>
-                               </div>
-                            </div>
-                         </div>
-                         <div class="order-notes">
-                            <div class="checkout-form-list">
-                               <label>Order Notes</label>
-                               <textarea id="checkout-mess" cols="30" rows="10"
-                                  placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
-                            </div>
-                         </div>
-                      </div>
+           
                    </div>
                 </div>
                 <div class="col-lg-6">
