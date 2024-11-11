08765df84324e34d1782cf69c895271bc1f316c5
Deepak2241993 <deepakprasad224@gmail.com>
2024-11-11 14:52:15 +0530

    page update

--- resources/views/gift/gift_purchase_payment_history.blade.php
+++ resources/views/gift/gift_purchase_payment_history.blade.php
@@ -1,30 +1,25 @@
 @extends('layouts.admin_layout')
 @section('body')
 
-<main class="app-main">
-    <!--begin::App Content Header-->
-    <div class="app-content-header">
-        <!--begin::Container-->
-        <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-                <div class="col-sm-6">
-                    <h3 class="mb-0">Giftcard Payment Review</h3>
-                   
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-end">
-                        <li class="breadcrumb-item"><a href="{{url('admin-dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">
-                            Payment Review
-                        </li>
-                    </ol>
-                </div>
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h3 class="mb-0">Service Order History</h3>
             </div>
-            <!--end::Row-->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Giftcard Payment Review</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">
+                        Payment Review
+                    </li>
+                </ol>
+            </div>
         </div>
-        <!--end::Container-->
-    </div>
+    </div><!-- /.container-fluid -->
+</section>
+<section class="app-main">
+    
     <!--end::App Content Header-->
     @if(isset($result))
     <!--begin::App Content-->
@@ -93,7 +88,7 @@
     @else
     <h1>No Data Found</h1>
     @endif
-</main>
+</section>
 
   @endsection
 
