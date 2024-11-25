4c58f240bc66a2c30f7ac205aee2e7866a0de104
Deepak2241993 <deepakprasad224@gmail.com>
2024-11-25 10:48:07 +0530

    remove order button from order history

--- resources/views/gift/order_history.blade.php
+++ resources/views/gift/order_history.blade.php
@@ -54,12 +54,12 @@
                             <td>{{ $loop->iteration }}</td>
                             <td>{{ $value->order_id }}</td>
                             <td>
-                                <a type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                {{-- <a type="button" class="btn btn-primary" data-bs-toggle="modal"
                                     data-bs-target="#staticBackdrop_{{ $value->id }}"
                                     onclick="OrderView({{ $key }}, '{{ $value->order_id }}')">
                                     Orders
                                 </a>
-                                |
+                                | --}}
                                 <a class="btn btn-warning"
                                     href="{{ route('service-invoice', ['transaction_data' => $value->id]) }}">
                                     Invoice
