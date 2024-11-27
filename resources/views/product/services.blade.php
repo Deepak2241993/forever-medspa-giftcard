59b87647889784da0da5a13a21747d88ab8d92ee
Deepak2241993 <deepakprasad224@gmail.com>
2024-11-27 11:55:20 +0530

    update

--- resources/views/product/services.blade.php
+++ resources/views/product/services.blade.php
@@ -216,6 +216,7 @@
                     <div class="row gy-50">
                         <div class="col-xxl-8 col-lg-8">
                             <h2>Black Friday Deals 2024</h2>
+                            
                             <div class="postbox__wrapper">
                                 @if (isset($data))
                                     @foreach ($data as $value)
