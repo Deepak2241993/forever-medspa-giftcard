aec116185ad7e1093af8713e8540e13abae7a32e
Deepak2241993 <deepakprasad224@gmail.com>
2024-12-12 16:02:40 +0530

    update

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -9,6 +9,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
+  <link rel="shortcut icon" href="http://localhost/medspa-program-management/public/medspa.png" type="image/x-icon" />
   <link rel="stylesheet" href="{{url('/')}}/plugins/fontawesome-free/css/all.min.css">
   <!-- IonIcons -->
   {{-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> --}}
