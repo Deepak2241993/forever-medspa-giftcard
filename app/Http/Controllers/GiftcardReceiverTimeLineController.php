665afc933aeee8c0687ef791a8d116a2d8cfa2cd
Temz@2025 <sushobhan@thetemz.com>
2025-02-03 17:59:41 +0530

    event lisiner and event generate

---
+++ app/Http/Controllers/GiftcardReceiverTimeLineController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GiftcardReceiverTimeLine;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GiftcardReceiverTimeLineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GiftcardReceiverTimeLine  $giftcardReceiverTimeLine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GiftcardReceiverTimeLine $giftcardReceiverTimeLine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GiftcardReceiverTimeLine  $giftcardReceiverTimeLine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GiftcardReceiverTimeLine $giftcardReceiverTimeLine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GiftcardReceiverTimeLine  $giftcardReceiverTimeLine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GiftcardReceiverTimeLine $giftcardReceiverTimeLine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GiftcardReceiverTimeLine  $giftcardReceiverTimeLine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GiftcardReceiverTimeLine $giftcardReceiverTimeLine)
+    {
+        //
+    }
+}
